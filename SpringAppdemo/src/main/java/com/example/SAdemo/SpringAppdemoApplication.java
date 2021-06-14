32ac8e65e38f01417143b6b9654b762d889fc8a0
Raashika88 <67578906+Raashika88@users.noreply.github.com>
2021-06-14 20:16:55 +0530

    complete filder of springappdemo

---
+++ SpringAppdemo/src/main/java/com/example/SAdemo/SpringAppdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SAdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppdemoApplication.class, args);
+	}
+
+}
---
+++ SpringAppdemo/src/test/java/com/example/SAdemo/SpringAppdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SAdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
